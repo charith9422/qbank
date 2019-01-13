96ec36b24b2a3dff49f28b9b6a1a10b1911ba764
charith9422 <35757692+charith9422@users.noreply.github.com>
2019-01-13 12:08:28 +0530

    Google login issue fixed

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -8,22 +8,22 @@ import {
 } from "@angular/fire/firestore";
 import { Observable, of } from "rxjs";
 import { switchMap } from "rxjs/operators";
-import { User } from "src/app/models/models";
+import { User } from "./user";
 
 @Injectable({
   providedIn: "root"
 })
 export class AuthService {
-  user: Observable<User>;
+  user$: Observable<User>;
   constructor(
     public afAuth: AngularFireAuth,
     private afs: AngularFirestore,
     private router: Router
   ) {
-    this.user = this.afAuth.authState.pipe(
+    this.user$ = this.afAuth.authState.pipe(
       switchMap(user => {
         if (user) {
-          return this.afs.doc<User>("users/${user.uid}").valueChanges();
+          return this.afs.doc<User>('users/${user.uid}').valueChanges();
         } else {
           return of(null);
         }
@@ -43,14 +43,15 @@ export class AuthService {
 
   private updateUserData(user) {
     const userRef: AngularFirestoreDocument<any> = this.afs.doc(
-      "users/${user.uid}"
+      'users/${user.uid}'
     );
 
     const data: User = {
       uid: user.uid,
       email: user.email,
-      registrationNo: user.registrationNo,
-      password: user.password
+      roles:{
+        subscriber:true
+      }
     };
 
     return userRef.set(data, { merge: true });
